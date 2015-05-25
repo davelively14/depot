8d9aba8e9ab005d230262f3d514aca256b7efe85
Dave Lively <davelively14@gmail.com>
2015-05-25 17:01:08 -0400

    Tried to add blinds functionality (does not work with tables, contrary to what the book says). Hides empty cart. Does not flash when cart is emptied.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # This helper will return block of html code with certain attributes.  One attribute to be added would be "display:
+  # none" if the passed condition is true (in this case, if the cart is empty).
+  def hidden_dev_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+
+    # Rails standard helper content_tag() can be used to wrap the output created by a block in a specific tag. In this
+    # case, content_tag() will wrap the passed block in a "div" tag.
+    content_tag("div", attributes, &block)
+  end
 end
