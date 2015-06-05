44801bcbba2fb2702fb1db11d22d72a6b8c5fe76
Dave Lively <davelively14@gmail.com>
2015-06-04 22:42:28 -0400

    Iteration J1: Selecting the locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
 
   # Prevent CSRF attacks by raising an exception.
@@ -7,9 +8,29 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
+  # Sets the locale from the params, but only if htere is a locale in the params. Otherwise, it leaves the current
+  # locale alone.
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
       end
     end
+  end
+
+  # Provides a hash of URL options that are to be considered as present whenever they aren't otherwise provided. In
+  # this case, we are providing a value for the :locale parameter. This is needed when a view on a page that does not
+  # have the locale specified attempts to construct a link to a page that does.
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,28 @@ Rails.application.routes.draw do
   end
 
   get 'sessions/create'
-
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-
-  get 'store/index'
 
   resources :products do
     get :who_bought, on: :member
+  end
+
+  # Nested resources and root declarations inside a scope declaration for :locale.  :locale is in parentheses, which
+  # is the way to indicate it's optional. Leaving admin and session functions outside of the scope because we will not
+  # translate them now. 'en' is
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
