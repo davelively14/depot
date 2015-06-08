7c8d7c0831c51c4bcec0ef6c5eeeee19a2fa3b9e
Dave Lively <davelively14@gmail.com>
2015-06-07 21:10:02 -0400

    Task J complete: Orders can now be completed in multiple languages. Right now, just EN and ES enabled.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,6 +6,12 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.order(:title)
+
+    # Will redirect to the store path for a given locale if the :set_locale form is used.
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
 end
